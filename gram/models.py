3a6174a3218b6d3ca92018da5123ec1d40c3b506
David <dgkilolo23@gmail.com>
2020-05-29 19:39:00 +0300

    add image model class.

--- gram/models.py
+++ gram/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Image(models.Model):
   picture = models.ImageField(upload_to='gram/')
-  name = models.CharField(max_length=30)
-  caption = models.TextField()
+  name = models.CharField(max_length=30, blank=True)
+  caption = models.TextField(blank=True)
+  date = models.DateTimeField(auto_now_add=True)
--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+  # Set up method
+  def setUp(self):
+    self.image = Image(name="dog picture", caption="man's best friend")
+  # Testing instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.image,Image))
