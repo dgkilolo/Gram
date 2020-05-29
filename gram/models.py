fcdc149ad5c5c92f5ae820db2c2a2625a3148da2
David <dgkilolo23@gmail.com>
2020-05-29 18:15:59 +0300

    add pillow library to enable use of the ImageField for uploading images.

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  picture = models.ImageField(upload_to='gram/')
+  name = models.CharField(max_length=30)
+  caption = models.TextField()
